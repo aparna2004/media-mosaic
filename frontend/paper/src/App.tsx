f86903bfa6e0ba1d7c17feb8decda846ea1ea2b8
Aparna Sathyamurthi Thambu <aparna.thambu@gmail.com>
2025-03-28 10:05:44 +0530

    Add tanstack

--- frontend/paper/src/App.tsx
+++ frontend/paper/src/App.tsx
@@ -1,32 +1,34 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import TopNews from './pages/TopNews';
 import Sports from './pages/Sports';
 import Finance from './pages/Finance';
 import Entertainment from './pages/Entertainment';
-import GitHubRedirect from './components/GitHubRedirect';
 import Technology from './pages/Technology';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<TopNews />} />
-            <Route path="/sports" element={<Sports />} />
-            <Route path="/finance" element={<Finance />} />
-            <Route path="/entertainment" element={<Entertainment />} />
-            <Route path="/technology" element={<Technology />} />  {/* Changed from tech */}
-            <Route path="/ajay" element={<GitHubRedirect username="hajay180505" />} />
-            <Route path="/aparna" element={<GitHubRedirect username="aparna2004" />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <Navbar />
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<TopNews />} />
+              <Route path="/sports" element={<Sports />} />
+              <Route path="/finance" element={<Finance />} />
+              <Route path="/entertainment" element={<Entertainment />} />
+              <Route path="/technology" element={<Technology />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
